3aadfd57be2c64fe09c729e63055ddc56d7625f2
hongbiao_chen <chenhongkwan@foxmail.com>
2017-04-21 18:35:20 +0800

    17.04.21

--- index/js/goAssess.js
+++ index/js/goAssess.js
@@ -1,14 +1,22 @@
-$(function(){
-	function star(){
-		$('.star').on('touchstart',function(){
-			var index = $(this).index();
-			console.log(index);
-			//$('.star:lt('+index+')').attr('src','goAssess_images/redstarts.png');
-			for(var i = 0;i < index;i++){
-				$('.star').eq(i).attr('src','goAssess_images/redstarts.png');
+$(function() {
+	//点击切换好评，中评，差评
+	function toAssess(){
+		$('.assessPic').on('touchstart',function(){
+			var choseSrc = $(this).attr('src');
+			if(choseSrc === "goAssess_images/aa@2x.png"){
+				$(this).attr('src','goAssess_images/good@2x.png').siblings('.assessPic').attr('src','goAssess_images/aa@2x.png')
 			}
 		})
 	}
+	//五角星评分
+	function star() {
+		$('.star').on('touchstart',function(){
+			$(this).attr('src','goAssess_images/redstarts.png');
+			$(this).prevAll().attr('src','goAssess_images/redstarts.png');
+			$(this).nextAll().attr('src','goAssess_images/starts.png');
+		})
+	}
 //函数调用
+toAssess();
 star();
 })
